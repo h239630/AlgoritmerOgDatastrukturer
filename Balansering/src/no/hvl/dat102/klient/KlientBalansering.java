56d58f69501088d3d9d4b9d27baf29e39dbbb2f6
Mikael Lindebrekke <lindebrekkem@gmail.com>
2020-03-24 10:57:48 +0100

    Fix!

--- Balansering/src/no/hvl/dat102/klient/KlientBalansering.java
+++ Balansering/src/no/hvl/dat102/klient/KlientBalansering.java
@@ -4,7 +4,7 @@ import no.hvl.dat102.Balansering;
 
 public class KlientBalansering{
      public static void main(String[] args){
-        final String filnavn = "C:\\DAT102\\Balansering\\src\\no\\hvl\\dat102\\Klient\\HelloWorld.java";
+        final String filnavn = "C:\\DAT102\\AlgoritmerOgDatastrukturer\\Balansering\\src\\no\\hvl\\dat102\\Klient\\SummerOppTil.java";
         //Leser inn en tekst fra fil
         Balansering balansering = new Balansering();
         balansering.lesFraFil(filnavn);
